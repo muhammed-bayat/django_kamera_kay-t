d5d9519ca7dccb1115974e5b84250edcfd14aefa
muhammed-bayat <muhammed.bayat07@erzurum.edu.tr>
2022-04-04 17:12:57 +0300

    fix

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('opencvdjango.urls')),
+    path('', include('apps.opencvdjango.urls')),
     
 ]
 
